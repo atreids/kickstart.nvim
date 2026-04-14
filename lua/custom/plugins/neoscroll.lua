68fc2d8bf3de5e299c9a0b18ebd9d6514e4b4b37
Aaron Donaldson <dev@adonaldson.com>
2026-04-14 21:18:11 +0100

    Changed neoscroll to linear

--- lua/custom/plugins/neoscroll.lua
+++ lua/custom/plugins/neoscroll.lua
@@ -19,7 +19,7 @@ return {
       respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
       duration_multiplier = 1.0, -- Global duration multiplier
-      easing = 'cubic', -- Default easing function
+      easing = 'linear', -- Default easing function
       pre_hook = nil, -- Function to run before the scrolling animation starts
       post_hook = nil, -- Function to run after the scrolling animation ends
       performance_mode = false, -- Disable "Performance Mode" on all buffers.
